ea0644ed822a2081c2e5198f8023c53922046706
Hire me, please! <87432830+phoenixArise@users.noreply.github.com>
2023-06-13 02:18:10 -0400

    Initial Commit

---
+++ MakeItEasy/MakeItEasyApp.swift
@@ -0,0 +1,20 @@
+//
+//  MakeItEasyApp.swift
+//  MakeItEasy
+//
+//  Created by Brian Seo on 2023-06-13.
+//
+
+import SwiftUI
+
+@main
+struct MakeItEasyApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
